f3271ffac4824458c4d6d76e84ba66b3ac081ab5
bmwtsn098 <bmwtsn99@gmail.com>
2022-10-24 00:33:13 -0700

    updated fuzz function

--- mayhem/fuzz.go
+++ mayhem/fuzz.go
@@ -5,6 +5,7 @@ import "github.com/ansible/receptor/pkg/logger"
 import "github.com/ansible/receptor/pkg/utils"
 import "github.com/ansible/receptor/pkg/backends"
 import "github.com/ansible/receptor/pkg/netceptor"
+import "github.com/ansible/receptor/tests/functional/lib/receptorcontrol"
 
 func mayhemit(bytes []byte) int {
 
@@ -32,6 +33,35 @@ func mayhemit(bytes []byte) int {
         case 3:
             var test backends.UDPDialerSession
             test.Send(bytes)
+            return 0
+
+        case 4:
+            var test backends.TCPSession
+            test.Send(bytes)
+            return 0
+
+        case 5:
+            content := string(bytes)
+            var test netceptor.Netceptor
+            test.ListenPacket(content)
+            return 0
+
+        case 6:
+            content := string(bytes)
+            var test receptorcontrol.ReceptorControl
+            test.Connect(content)
+            return 0
+
+        case 7:
+            content := string(bytes)
+            var test receptorcontrol.ReceptorControl
+            test.Ping(content)
+            return 0
+
+        case 8:
+            content := string(bytes)
+            var test receptorcontrol.ReceptorControl
+            test.WorkSubmitJSON(content)
             return 0
 
         default:
